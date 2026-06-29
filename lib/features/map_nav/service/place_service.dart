9f55362ba325df916fa87dfcdd9a9759d326e5e7
krish-solanki <ksolanki700@rku.ac.in>
2026-06-30 01:34:24 +0530

    Modifie code of send request throw api

--- lib/features/map_nav/service/place_service.dart
+++ lib/features/map_nav/service/place_service.dart
@@ -1,17 +1,30 @@
 import 'package:eco_route/models/place_model.dart';
+import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PlaceService {
   Future<List<PlaceModel>> searchPlace(String query) async {
-    final responce = await http.get(
+    final response = await http.get(
       Uri.parse(
-        'https://nominatim.openstreetmap.org/search?q=$query&format=jsonv2',
+        'https://nominatim.openstreetmap.org/search'
+        '?q=$query'
+        '&format=jsonv2'
+        '&countrycodes=in'
+        '&limit=10',
       ),
-      headers: {'User-Agent': 'EcoRoute'},
+      headers: {'User-Agent': 'EcoRoute/1.0'},
     );
 
-    final data = jsonDecode(responce.body);
-    return (data as List).map((e) => PlaceModel.fromJson(e)).toList();
+    debugPrint("Status: ${response.statusCode}");
+    debugPrint(response.body);
+
+    if (response.statusCode != 200) {
+      throw Exception("Request failed");
+    }
+
+    final List data = jsonDecode(response.body);
+
+    return data.map((e) => PlaceModel.fromJson(e)).toList();
   }
 }
